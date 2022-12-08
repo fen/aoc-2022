45bc997fc41f29592536cc2b909088d7685a4608
Fredrik Eriksson <fredrik@eriksson.tech>
2022-12-08 08:59:52 +0100

    Cleanup day8 and rename cell to tree.

--- day8/Program.cs
+++ day8/Program.cs
@@ -1,47 +1,48 @@
-﻿if (args is not [var file] || !System.IO.File.Exists(file)) {
+﻿if (args is not [var file] || !File.Exists(file)) {
     Console.WriteLine("day8 <input>");
     return;
 }
 
 var grid = await Grid.ParseAsync(file);
 
-var edges = grid.RowCount * 2 + grid.ColumnCount * 2;
-Console.WriteLine("Part One: {0}", grid.GetCells().Count(c => c.IsVisible()) + edges);
-Console.WriteLine("Part Two: {0}", grid.GetCells().Max(c => c.GetScenicScore()));
+var edges = (grid.RowCount * 2 + grid.ColumnCount * 2)-4; // -4 so we don't double count the edges
+Console.WriteLine(edges);
+Console.WriteLine("Part One: {0}", grid.GetTrees().Count(c => c.IsVisible()) + edges);
+Console.WriteLine("Part Two: {0}", grid.GetTrees().Max(c => c.GetScenicScore()));
 
 class Grid
 {
-    private List<Tree> Cells { get; } = new();
+    private List<Tree> Trees { get; } = new();
 
     public int RowCount { get; private set; }
     public int ColumnCount { get; private set; }
 
     private void Add(Tree tree)
     {
-        RowCount = Math.Max(RowCount, tree.Row);
-        ColumnCount = Math.Max(ColumnCount, tree.Column);
-        Cells.Add(tree);
+        RowCount = Math.Max(RowCount, tree.Row+1);
+        ColumnCount = Math.Max(ColumnCount, tree.Column+1);
+        Trees.Add(tree);
     }
 
-    public Tree GetCell(int row, int column) => Cells[(row * (RowCount + 1)) + column];
+    public Tree GetTree(int row, int column) => Trees[row * RowCount + column];
 
-    public IEnumerable<Tree> GetCells()
+    public IEnumerable<Tree> GetTrees()
     {
-        foreach (var cell in Cells) {
-            if (cell.Row == 0 || cell.Column == 0 || cell.Row == RowCount || cell.Column == ColumnCount) {
+        foreach (var cell in Trees) {
+            if (IsEdge(cell)) {
                 continue;
             }
-
             yield return cell;
         }
+
+        bool IsEdge(Tree tree) => tree.Row == 0 || tree.Row == RowCount - 1 || tree.Column == 0 ||
+                                  tree.Column == ColumnCount - 1;
     }
 
     public static async Task<Grid> ParseAsync(string file)
     {
         var grid = new Grid();
-
         var rows = await File.ReadAllLinesAsync(file);
-        List<Tree> cells = new();
         for (var rowNumber = 0; rowNumber < rows.Length; rowNumber++) {
             var row = rows[rowNumber];
             for (var columnNumber = 0; columnNumber < row.Length; columnNumber++) {
@@ -76,9 +77,9 @@ record Tree(int Value, int Row, int Column, Grid Grid)
     {
         var currentRow = Row + row;
         var currentColumn = Column + column;
-        while (currentColumn >= 0 && currentColumn <= Grid.ColumnCount && currentRow >= 0 &&
-               currentRow <= Grid.RowCount) {
-            yield return Grid.GetCell(currentRow, currentColumn);
+        while (currentColumn >= 0 && currentColumn < Grid.ColumnCount && currentRow >= 0 &&
+               currentRow < Grid.RowCount) {
+            yield return Grid.GetTree(currentRow, currentColumn);
             currentRow += row;
             currentColumn += column;
         }
