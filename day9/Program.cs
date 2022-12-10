c6571a1e967575ace5f10cc40dac474b216d27b7
Fredrik Eriksson <fredrik@eriksson.tech>
2022-12-10 11:55:53 +0100

    Cleanup the solution for day9.

--- day9/Program.cs
+++ day9/Program.cs
@@ -11,14 +11,14 @@ void PartOne()
 {
     var simulator = new Simulator(2);
     simulator.Run(movements);
-    Console.WriteLine("Part One: {0}", simulator.Knots.Last().Recorded.Count);
+    Console.WriteLine("Part One: {0}", simulator.Recorded.Count);
 }
 
 void PartTwo()
 {
     var simulator = new Simulator(10);
     simulator.Run(movements);
-    Console.WriteLine("Part Two: {0}", simulator.Knots.Last().Recorded.Count);
+    Console.WriteLine("Part Two: {0}", simulator.Recorded.Count);
 }
 
 static async Task<List<Movement>> ParseAsync(string file)
@@ -26,139 +26,72 @@ static async Task<List<Movement>> ParseAsync(string file)
 
 class Simulator
 {
-    public Simulator(int numberOfKnots)
+    Position[] _knots;
+
+    public Simulator(int numberOfKnots) => _knots = new Position[numberOfKnots];
+    public HashSet<Position> Recorded { get; } = new() { new() };
+
+    public void Run(List<Movement> movements)
     {
-        Knots = new List<Knot>(Enumerable.Range(0, numberOfKnots).Select(_ => new Knot()));
-    }
+        foreach (var movement in movements) {
+            foreach (var step in Steps(_knots[0], movement)) {
+                _knots[0] = step;
 
-    public List<Knot> Knots { get; }
-
-    public void Run(List<Movement> movements) => movements.ForEach(Run);
-
-    private void Run(Movement movements)
-    {
-        var head = Knots.First();
-        foreach (var step in Steps(head.Position, movements)) {
-            head.Position = step;
-
-            for (var i = 1; i < Knots.Count; i++) {
-                var current = Knots[i];
-                var other = Knots[i - 1];
-                if (!current.Position.IsNextTo(other.Position)) {
-                    current.Position = current.Position.GetPosition(other.Position);
+                var last = _knots.Length - 1;
+                for (var i = 1; i < _knots.Length; i++) {
+                    var newPosition = _knots[i].Move(_knots[i - 1]);
+                    if (_knots[i] != newPosition) {
+                        if (i == last) Recorded.Add(newPosition);
+                        _knots[i] = newPosition;
+                    }
                 }
             }
         }
     }
 
-    private IEnumerable<Position> Steps(Position start, Movement movement)
+    IEnumerable<Position> Steps(Position start, Movement movement)
     {
-        Position p = movement.Direction switch {
-            Direction.Left => new(-1, 0),
-            Direction.Right => new(+1, 0),
-            Direction.Up => new(0, +1),
-            Direction.Down => new(0, -1),
-            _ => throw new ArgumentOutOfRangeException()
-        };
-
-        var current = start;
-        for (int i = 0; i < movement.Count; i++) {
-            current += p;
-            yield return current;
-        }
+        for (int i = 0; i < movement.Count; i++)
+            yield return start += movement.Position;
     }
-}
-
-class Knot
-{
-    private Position _position;
-
-    public Knot()
-    {
-        Recorded.Add(Position);
-    }
-
-    public Position Position {
-        get => _position;
-        set {
-            _position = value;
-            Recorded.Add(value);
-        }
-    }
-
-    public HashSet<Position> Recorded { get; } = new();
 }
 
 record struct Position(int X, int Y)
 {
-    private static readonly Position[] Movements = {
-        new(-1, 0),  // left
-        new(+1, 0),  // right
-        new(0, +1),  // up
-        new(0, -1),  // down
-
-        new(-1, -1), // bottom-left
-        new(+1, -1), // bottom-right
-        new(+1, +1), // top-right
-        new(-1, +1), // top-left
-    };
-
-    public bool IsNextTo(Position other, bool isDirectlyNextTo = false)
+    public Position Move(Position o)
     {
-        if (Equals(other)) return true;
-        foreach (var position in Movements.Take(isDirectlyNextTo ? 4 : 8)) {
-            if (this + position == other) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public Position GetPosition(Position other)
-    {
-        foreach (var position in Movements) {
-            if ((this + position).IsNextTo(other, isDirectlyNextTo: true)) {
-                return this + position;
-            }
-        }
-
-        foreach (var position in Movements) {
-            if ((this + position).IsNextTo(other, isDirectlyNextTo: false)) {
-                return this + position;
-            }
-        }
-
-        throw new();
+        var x = (o - this);
+        var p = x.Abs();
+        if (p.X > 1 || p.Y > 1)
+            return this + x / p;
+        return this;
     }
 
     public static Position operator +(Position left, Position right)
         => new(X: left.X + right.X, Y: left.Y + right.Y);
+    public static Position operator -(Position left, Position right)
+        => new(X: left.X - right.X, Y: left.Y - right.Y);
+
+    public static Position operator /(Position left, Position right)
+        => new(X: left.X / Math.Max(right.X, 1), Y: left.Y / Math.Max(right.Y, 1));
+
+    public Position Abs() => new(Math.Abs(X), Math.Abs(Y));
 }
 
-record Movement(Direction Direction, int Count)
+record Movement(Position Position, int Count)
 {
     public static Movement Parse(string value)
     {
         var (direction, count) = value.Split(' ');
-
         return new(
             direction switch {
-                "L" => Direction.Left,
-                "R" => Direction.Right,
-                "U" => Direction.Up,
-                "D" => Direction.Down,
-                _ => throw new("unknown direction")
+                "L" => new(-1, 0),
+                "R" => new(+1, 0),
+                "U" => new(0, +1),
+                "D" => new(0, -1),
+                _ => throw new()
             },
             int.Parse(count)
         );
     }
-}
-
-enum Direction
-{
-    Left,
-    Right,
-    Up,
-    Down
 }
