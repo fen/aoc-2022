cbb27d885b7b227893117028da935407748d7329
Fredrik Eriksson <fredrik@eriksson.tech>
2022-12-13 18:58:05 +0100

    Day 13

---
+++ day13/Program.cs
@@ -0,0 +1,108 @@
+﻿if (args is not [var file] || !File.Exists(file)) {
+    Console.WriteLine("day13 <input>");
+    return;
+}
+
+var input = await ReadAsync(file);
+
+Console.WriteLine(
+    "Part One: {0}",
+    input.ByTwo()
+        .Select(((p, i) => Comparer.Default.Compare(p.First, p.Second) < 0 ? i + 1 : 0))
+        .Sum()
+);
+
+var first = List.New(List.New(2));
+var second = List.New(List.New(6));
+input.Add(first);
+input.Add(second);
+Console.WriteLine(
+    "Part Two: {0}",
+    input.Order(Comparer.Default)
+        .Select((item, index) => (item, index))
+        .Where(n => ReferenceEquals(n.item, first) || ReferenceEquals(n.item, second))
+        .Select(n => n.index+1).Product()
+);
+
+static async Task<List<Item>> ReadAsync(string file)
+{
+    List<Item> result = new();
+    var input = await File.ReadAllTextAsync(file);
+    var pairs = input.Split("\r\n\r\n");
+    foreach (var pair in pairs) {
+        var (one, two) = pair.Split("\r\n");
+        int index = 0;
+        result.Add(Parse(one, ref index));
+        index = 0;
+        result.Add(Parse(two, ref index));
+    }
+
+    return result;
+
+    static Item Parse(ReadOnlySpan<char> input, ref int index)
+    {
+        while (index < input.Length) {
+            if (input[index] == '[') {
+                index += 1;
+                var currentList = new List(new());
+                while (input[index] != ']') {
+                    currentList.Items.Add(Parse(input, ref index));
+                    if (input[index] == ',') index += 1; // skip ,
+                }
+
+                index += 1;
+                return currentList;
+            }
+
+            if (char.IsNumber(input[index])) {
+                int start = index++, count = 1;
+                while (char.IsNumber(input[index])) {
+                    index += 1;
+                    count += 1;
+                }
+
+                return int.Parse(input.Slice(start, count));
+            }
+        }
+
+        throw new();
+    }
+}
+
+sealed class Comparer : IComparer<Item>
+{
+    public static readonly Comparer Default = new();
+
+    public int Compare(Item? l, Item? r) => (l, r) switch {
+        (Number nl, Number nr) => nl.Value.CompareTo(nr.Value),
+        (List ll, Number nr) => Compare(ll, List.New(nr)),
+        (Number nl, List lr) => Compare(List.New(nl), lr),
+        (List { IsEmpty: true }, List { IsEmpty: true }) => 0,
+        (List { IsEmpty: true }, List { IsEmpty: false }) => -1,
+        (List { IsEmpty: false }, List { IsEmpty: true }) => 1,
+        (List and [var a, .. var rl], List and [var b, .. var rr])
+            => Compare(a, b) switch { 0 => Compare(rl, rr), var c => c },
+        _ => throw new()
+    };
+}
+
+abstract record Item
+{
+    public static implicit operator Item(int value) => new Number(value);
+}
+
+record Number(int Value) : Item
+{
+    public static implicit operator Number(int value) => new(value);
+    public override string ToString() => Value.ToString();
+}
+
+record List(List<Item> Items) : Item
+{
+    public static List New(Item item) => new(new List<Item> { item });
+    public int Count => Items.Count;
+    public Item this[int index] => Items[index];
+    public List Slice(int start, int length) => new(new List<Item>(Items.GetRange(start, length)));
+    public bool IsEmpty => Items.Count == 0;
+    public override string ToString() => $"[{string.Join(',', Items)}]";
+}
--- help/Helpers.cs
+++ help/Helpers.cs
@@ -6,6 +6,21 @@ internal static class AdventOfCodeHelpers
     /// <summary>
     /// Group IEnumerable sequence in a enumerable tuple of three items.
     /// </summary>
+    public static IEnumerable<(T First, T Second)> ByTwo<T>(this IEnumerable<T> seq)
+    {
+        using var enumerator = seq.GetEnumerator();
+        do {
+            if (!enumerator.MoveNext()) yield break;
+            var first = enumerator.Current;
+            enumerator.MoveNext();
+            var second = enumerator.Current;
+            yield return (first, second);
+        } while (true);
+    }
+
+    /// <summary>
+    /// Group IEnumerable sequence in a enumerable tuple of three items.
+    /// </summary>
     public static IEnumerable<(T first, T second, T third)> ByThree<T>(this IEnumerable<T> seq)
     {
         using var enumerator = seq.GetEnumerator();
@@ -23,11 +38,7 @@ internal static class AdventOfCodeHelpers
     public static void Deconstruct<T>(this T[] seq, out T first, out T second)
     {
         first = seq[0];
-        if (seq.Length == 1) {
-            second = default;
-        } else {
-            second = seq[1];
-        }
+        second = seq.Length == 1 ? default! : seq[1];
     }
 
     public static bool TryRemoveFirst<T>(this List<T> items, [NotNullWhen(true)] out T? item)
@@ -45,8 +56,8 @@ internal static class AdventOfCodeHelpers
 
     public static long Product(this IEnumerable<long> source)
         => Product<long, long>(source);
-    public static UInt128 Product(this IEnumerable<UInt128> source)
-        => Product<UInt128, UInt128>(source);
+    public static int Product(this IEnumerable<int> source)
+        => Product<int, int>(source);
 
     private static TResult Product<TSource, TResult>(this IEnumerable<TSource> source)
         where TSource : struct, INumber<TSource>
